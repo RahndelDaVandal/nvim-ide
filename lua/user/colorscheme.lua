f6269eb7f4fed000706825989687097b75388117
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-08-19 08:24:48 -0500

    more tweaking

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
---local colorscheme = "darkplus"
-local colorscheme = "nord"
+-- local colorscheme = "darkplus"
+-- local colorscheme = "nord"
+-- local colorscheme = "dracula"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
